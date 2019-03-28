aae77ca1455f1f9934773d72731d2463c7712acf
Flipza <philipp.zahner@gmx.ch>
2019-03-28 09:54:17 +0100

    Updated Loginsystem

--- logout.php
+++ logout.php
@@ -1,23 +1,14 @@
 <?php
-// Session starten
+// Initialize the session
 session_start();
-session_regenerate_id();
-
-// Session leeren
+ 
+// Unset all of the session variables
 $_SESSION = array();
-
-// Cookie löschen
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000, $params["path"],
-        $params["domain"], $params["secure"], $params["httponly"]
-    );
-}
-
-// Session löschen
+ 
+// Destroy the session.
 session_destroy();
-
-// Weiterleitung auf anmeldung
-header('Location: index.php');
-
+ 
+// Redirect to login page
+header("location: login.php");
+exit;
 ?>
